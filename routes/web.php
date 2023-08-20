f4c22742c153b0f6709cc31bbdf43199414875a1
et-Ghifari <et.ghifari@gmail.com>
2023-08-20 09:51:45 +0700

    add views file and code route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('app.blank-page', ['type_menu' => '']);
+    return view('apps.ecommerce-dashboard', ['type_menu' => '']);
+});
+
+// auth
+Route::get('/forgot-password', function () {
+    return view('auths.forgot-password', ['type_menu' => 'auths']);
+});
+Route::get('/login', function () {
+    return view('auths.login', ['type_menu' => 'auths']);
+});
+Route::get('/login2', function () {
+    return view('auths.login2', ['type_menu' => 'auths']);
+});
+Route::get('/register', function () {
+    return view('auths.register', ['type_menu' => 'auths']);
+});
+Route::get('/reset-password', function () {
+    return view('auths.reset-password', ['type_menu' => 'auths']);
+});
+
+// forms
+Route::get('/advanced-form', function () {
+    return view('forms.advanced-form', ['type_menu' => 'forms']);
+});
+Route::get('/editor', function () {
+    return view('forms.editor', ['type_menu' => 'forms']);
+});
+Route::get('/validation', function () {
+    return view('forms.validation', ['type_menu' => 'forms']);
 });
